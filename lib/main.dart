eb702af1c01baa8ea6238c8f77a551693d9fb2aa
Max1881 <85215401+Max1881@users.noreply.github.com>
2021-10-19 18:33:18 +0530

    new
    task

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(),
+      home: Scaffold(backgroundColor: Colors.amber),
     );
   }
 }
